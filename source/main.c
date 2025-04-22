f38ae135d9246998d97fd7e92e3701902f071724
Leotrim Sadikaj <leotrimsad@gmail.com>
2025-04-22 16:55:30 +0200

    okkkk

--- source/main.c
+++ source/main.c
@@ -6,7 +6,7 @@
 /*   By: lsadikaj <lsadikaj@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 13:59:25 by jimpa             #+#    #+#             */
-/*   Updated: 2025/04/10 15:10:49 by lsadikaj         ###   ########.fr       */
+/*   Updated: 2025/04/22 16:55:01 by lsadikaj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	main(int argc, char **argv, char **envp)
 	(void)argv;
 	char **my_envp;
 
-	my_envp =ft_array_dup(envp);
+	my_envp = ft_array_dup(envp);
 	setup_signals();
 	ft_read_line(&my_envp);
 
